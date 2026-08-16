7d6614fa11a23564c2ed151712e4db1d36ad74f3
gsadsadsa <dimti77@gmail.com>
2026-08-16 19:32:29 +0900

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+﻿function App() {
+    return (
+        <div>
+            <h1>WebProjectLimen</h1>
+
+            <p>캐릭터 월드 프로젝트</p>
+
+            <button>캐릭터 만들기</button>
+        </div>
+    )
+}
+
+export default App
