82c5339e5cef290ab0c497f21ce516feb76a1da0
gsadsadsa <dimti77@gmail.com>
2026-08-16 21:59:03 +0900

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
 
+    base: '/PROJECTLIMEN/',
+
     server: {
         watch: {
             ignored: ['**/.vs/**'],
